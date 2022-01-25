143b8a31abec401ba41c132bda55326a6f23461c
Kampojet <98385866+Kampojet@users.noreply.github.com>
2022-01-25 10:45:27 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,4 +1,28 @@
-//
-// Created by kamil on 25.01.2022.
-//
+#include <stdio.h>
+#include "dodawanie.h"
+#include "odejmowanie.h"
+#include "mnozenie.h"
+#include "dzielenie.h"
 
+int main(){
+    int znak, liczba1, liczba2;
+    printf("Wybierz operacje (1-dodawanie, 2-odejmowanie, 3-mnozenie, 4-dzielenie");
+    scanf("%d", &znak);
+    printf("Podaj dwie liczby");
+    scanf("%d", &liczba1);
+    scanf("%d", &liczba2);
+    switch (znak){
+        case 1;
+            dodawanie(liczba1, liczba2);
+            break;
+        case 2;
+            odejmowanie(liczba1, liczba2);
+            break;
+        case 3;
+            mnozenie(liczba1, liczba2);
+            break
+        case 4;
+            dzielenie(liczba1, liczba2);
+            break;
+    }
+}
